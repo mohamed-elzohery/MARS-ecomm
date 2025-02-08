fd192fe8668b8ec08e1e4011cfd9c93edb9734c6
mohamed elzohery <m.zohery1998@gmail.com>
2025-02-08 15:34:06 +0200

    feat: adding dummy product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import ProductsList from "@/components/shared/products/ProductList";
+import sampleData from "@/assets/db/sample-data";
 import React from "react";
+
+export type Product = (typeof sampleData.products)[0];
 
 const HomePage = async () => {
   return (
-    <div>
-      <h1>Mars Store</h1>
+    <div className="flex flex-col gap-4">
+      <ProductsList
+        title="Featured Products"
+        products={sampleData.products}
+        limit={10}
+      />
     </div>
   );
 };
---
+++ components/shared/products/ProductList.tsx
@@ -0,0 +1,33 @@
+import { Product } from "@/app/(root)/page";
+import React from "react";
+
+type ProductsListProps = {
+  title: string;
+  products: Product[];
+  limit?: number;
+};
+
+const ProductsList: React.FC<ProductsListProps> = ({
+  title,
+  products,
+  limit,
+}) => {
+  const limitedProducts = limit ? products.slice(0, limit) : products;
+  return (
+    <section className="my-10">
+      <h2 className="text-2xl h2-bold mb-5">{title}</h2>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        {limitedProducts.map((product) => (
+          <li
+            key={product.slug}
+            className="border border-gray-200 rounded-lg overflow-hidden"
+          >
+            {product.name}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProductsList;
