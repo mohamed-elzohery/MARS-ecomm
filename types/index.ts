7f70db0bd466ab4ff34515aa2222152b4a13d30f
mohamed elzohery <m.zohery1998@gmail.com>
2025-02-23 19:54:36 +0200

    feat(cart): adding database model

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
-import { productInsertionSchema } from '@/lib/validators';
+import { cartItemSchema, insertCartSchema, productInsertionSchema } from '@/lib/validators';
 import z from 'zod'
 export type Product = z.infer<typeof productInsertionSchema> & {rating: string,id: string, createdAt: Date};
+export type CartItem = z.infer<typeof cartItemSchema>;
+export type Cart = z.infer<typeof insertCartSchema>;
